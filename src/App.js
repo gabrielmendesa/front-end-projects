854e33b291a7573fdb25e889c9ebac04f8a1b68f
Gabriel Mendes <gabrielmendesa@gmail.com>
2022-02-01 16:25:49 -0300

    Requisito 3 concluido

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
@@ -8,8 +9,10 @@ class App extends React.Component {
       <div>
         <Header />;
         <SolarSystem />;
+        <Title />;
       </div>
     )
   }
+}
 
 export default App;
