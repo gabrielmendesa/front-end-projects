cb5b9bab4bbeec6b996a4034512484ed66cd0f2c
Gabriel Mendes <gabrielmendesa@gmail.com>
2022-02-01 20:05:10 -0300

    Requisito 10 concluido

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missons extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headLine="Missões" />
+        {missions.map((mission) => (
+          <MissionCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />
+        ))}
       </div>
     );
   }
